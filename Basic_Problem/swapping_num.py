c60c1715c1c6d7f22a7b26fb7ca1a87daf8c654e
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-08-31 19:20:44 +0530

    Swapping between 2 numbers

---
+++ Basic_Problem/swapping_num.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a :"))
+b = int(input("Enter b :"))
+
+print("Before Swapping :","a :",a," ","b :",b)
+
+temp = a
+a = b
+b = temp
+
+print("After Swapping :","a :",a," ","b :",b)
