d06d1079f241142bffcf6fb374731502ef077030
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-08-31 19:16:03 +0530

    Added check age eligible Program

---
+++ Basic_Problem/check_age_eligible.py
@@ -0,0 +1,6 @@
+name = input("Enter your name :")
+age = int(input("Enter your age :"))
+if(age >= 18):
+    print(f"{name} you are Eligible for Vote✅")
+else:
+    print(f"Sorry {name} you are not Eligible for Vote✖️")
