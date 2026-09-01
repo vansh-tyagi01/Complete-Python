ec85c3baf091f0a710a816acd76b9abd5dfbd65c
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-01 19:32:22 +0530

    Find greatest given 2 numbers

---
+++ Basic_Problem/greatest_2_num.py
@@ -0,0 +1,7 @@
+num_1 = int(input("Enter 1st number :"))
+num_2 = int(input("Enter 2nd number :"))
+
+if(num_1 > num_2):
+    print(f"{num_1} is Greatest.")
+else:
+    print(f"{num_2} is Greatest.")
