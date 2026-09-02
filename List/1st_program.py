282fb55d10ed103eb133413fac6cb463aeef94c2
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-02 15:55:31 +0530

    Add List 1st Program

---
+++ List/1st_program.py
@@ -0,0 +1,15 @@
+# List is a collection of different datatypes and it is Mutable.
+
+fruits = ['Mango','Apple','Orange','Pineapple']
+print("Fruits :",fruits)
+
+
+
+numbers = [25,50,75,100]
+print("Numbers :",numbers)
+
+
+
+num_fruits = ["Apple",25,"Mango",50]
+print(num_fruits)
+
