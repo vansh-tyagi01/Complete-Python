4a8fda64bc4b5f2f1929ee026d27990b544fe495
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-02 15:59:22 +0530

    Understanding append method

---
+++ List/List_append.py
@@ -0,0 +1,16 @@
+#******************************** APPEND ****************************************
+
+#  Append --> In any existing data add new data using .append() method in the last.
+
+# 1. Numbers Append
+
+num = [10,20,30]
+
+num.append(40)
+print(num)
+
+# 2. String Appned
+
+name = ['kanhiaya','payal']
+name.append('Arun')
+print(name)
