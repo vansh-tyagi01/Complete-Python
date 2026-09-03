a158fdadfec5e874b771b0c268ba6f05025e9be1
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-03 07:36:50 +0530

    Display even number b/w 1-100

---
+++ Loops/print_even_numbers.py
@@ -0,0 +1,19 @@
+# Using for Loop
+
+# 1 method
+for i in range(2 , 101 , 2):
+    print(i,end=" ")
+
+# 2nd method
+for i in range(1,101):
+    if i%2==0:
+        print(i,end=" ")
+
+
+# Using while loop
+
+i = 1
+while(i <= 100):
+    if i%2==0:
+        print(i,end=" ")
+    i+=1
