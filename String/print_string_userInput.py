9c19ae1329bb1dd82ec6d109c4bc0b50a281615f
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-04 08:04:55 +0530

    UserInput string and store in empty dictionary

---
+++ String/print_string_userInput.py
@@ -0,0 +1,19 @@
+# Print Name
+
+name = input("Enter any Name :")
+print("Name :",name)
+
+
+# Store Name , Branch , Father Name in Empty Dictionary and print.
+
+student_detail = {}
+
+stu_name = input("Enter student Name :")
+stu_branch = input("Enter student Branch :")
+stu_fatherName = input("Enter student Father Name :")
+
+student_detail["Student Name :"] = stu_name
+student_detail["Student Branch :"] = stu_branch
+student_detail["Student Father_Name :"] = stu_fatherName
+
+print("Student Detail :",student_detail)
