495a6889871e560949560d1a39b1dc78ae8bfa24
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-14 19:27:41 +0530

    Find Maximum 2 numbers

---
+++ Functions/Max2Numbers.py
@@ -0,0 +1,11 @@
+def Max(num1 , num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+num_1 = int(input("Enter 1st number :"))
+
+num_2 = int(input("Enter 2nd number :"))
+
+print("Maximum :",Max(num_1 , num_2))
