10a18c3618df3aa359a93b94ec39bd4e02605de1
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-16 17:26:27 +0530

    Check age eligible for vote

---
+++ Conditional Statements/check_ageVoting.py
@@ -0,0 +1,7 @@
+name = input("Enter your Name :")
+age = int(input("Enter your Age :"))
+
+if age>=18:
+    print(f"{name} you are eligible for Vote")
+else:
+    print(f"{name} You are not eligible for Vote")
