630803b860a818f5aaea932459953bc472af3b81
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-22 19:16:18 +0530

    pattern problem

---
+++ Pattern Problem/Solve pattern using while loop/sec_pattern.py
@@ -0,0 +1,18 @@
+num = int(input("Enter row of pattern :"))
+
+row = 1
+while(row <= num):
+    col = 1
+    while(col <= row):
+        print("*",end=" ")
+        col+=1
+    print()
+    row+=1
+
+# Output:
+        # Enter row of pattern : 4
+        
+        #   *
+        #   * *
+        #   * * *
+        #   * * * *
